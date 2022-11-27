de5d803560cd28c3b894f147ae779a2df4774dcd
Seth Winkhart <swinkhart58@gmail.com>
2022-11-27 00:07:27 -0500

    added a loop mechanism

--- KNN_anime_recommendation.py
+++ KNN_anime_recommendation.py
@@ -22,11 +22,41 @@ def animeRec(name, type=None):
 
 
 while True:
-    name, number, ty = input(
-        "Enter the name of the anime, number of recommendations, and type of anime(name number type), enter(d d d) to quit: "
-    ).split()
+    name = input("Enter the name of the anime: ")
+    number = int(input("Enter number of recommendations: "))
+    print("TV:1 | Movie:2 | Special:3 | OVA:4 | ONA:5 | None:0")
+    type_ = int(input("Enter number for type of anime: "))
 
+    #KDtree is used because it is faster in low dimensional datasets
     nbrs = NearestNeighbors(n_neighbors=number, algorithm="kd_tree").fit(fitDf)
     distances, indices = nbrs.kneighbors(fitDf)
 
-    animeRec("Toki wo Kakeru Shoujo")
+    try:
+        match type_:
+            case 0:
+                animeRec(name)
+            case 1:
+                animeRec(name, "TV")
+            case 2:
+                animeRec(name, "Movie")
+            case 3:
+                animeRec(name, "Special")
+            case 4:
+                animeRec(name, "OVA")
+            case 5:
+                animeRec(name, "ONA")
+            case _:
+                print("Invalid Input")
+    except IndexError:
+        print(f"!!!{name} was not present in the available dataset!!!")
+        again = input("would you like to go again?(y|n): ")
+        if again == "n":
+            break;
+        else:
+            continue
+
+    again = input("would you like to go again(y|n): ")
+    if again == "n":
+        break;
+
+print("===DONE===")
