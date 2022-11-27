6dd7c8caa51aeab343debce3ff831ac0b6350b00
swinkhart <swinkhart58@gmail.com>
2022-11-26 20:44:07 -0500

    added ability to pick types

--- KNN_anime_recommendation.py
+++ KNN_anime_recommendation.py
@@ -5,24 +5,28 @@ from sklearn.neighbors import NearestNeighbors
 mainDf = pd.read_csv("datasets/cleanedAnime.csv")
 fitDf = pd.read_csv("datasets/fitSet.csv")
 
-# using maxabsscalar now might change later after more resaerch
-max_abs_scaler = MaxAbsScaler()
-fitData = max_abs_scaler.fit_transform(fitDf)
-
-# gotta play around with this some more, may switch it up by calling the algorithm directly, ripped from scikit learn example
-nbrs = NearestNeighbors(n_neighbors=6, algorithm="auto").fit(fitDf)
-distances, indices = nbrs.kneighbors(fitDf)
-
-# need to make this less concise, E X P A N D
-def getIndexFromName(name):
-    return mainDf[mainDf["name"] == name].index[0]
-
-
 # need to modify to accept types(OVA, ONA, Movie, ect)
-def getSimlarAnimes(name, type=None):
-    found_id = getIndexFromName(name)
-    for id in indices[found_id][1:]:
-        print(mainDf.iloc[id]["name"])
+def animeRec(name, type=None):
+    if type:
+        found_id = mainDf[mainDf["name"] == name].index[0]
+        for id in indices[found_id][1:]:
+            dfToListDf = mainDf.loc[
+                (mainDf["Unnamed: 0"] == id) & (mainDf["type"] == type)
+            ]
+            for item in dfToListDf["name"].values.tolist():
+                print(item)
+    else:
+        found_id = mainDf[mainDf["name"] == name].index[0]
+        for id in indices[found_id][1:]:
+            print(mainDf.iloc[id]["name"])
 
 
-getSimlarAnimes("Majo no Takkyuubin")
+while True:
+    name, number, ty = input(
+        "Enter the name of the anime, number of recommendations, and type of anime(name number type), enter(d d d) to quit: "
+    ).split()
+
+    nbrs = NearestNeighbors(n_neighbors=number, algorithm="kd_tree").fit(fitDf)
+    distances, indices = nbrs.kneighbors(fitDf)
+
+    animeRec("Toki wo Kakeru Shoujo")
