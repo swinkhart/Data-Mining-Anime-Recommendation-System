a5affdb61a1f497c453b220abfcbf184a5e44a47
Seth Winkhart <swinkhart58@gmail.com>
2022-11-27 16:33:46 -0500

    changed the code to use a new dataset

--- KNN_anime_recommendation.py
+++ KNN_anime_recommendation.py
@@ -8,24 +8,25 @@ fitDf = pd.read_csv("datasets/fitSet.csv")
 # need to modify to accept types(OVA, ONA, Movie, ect)
 def animeRec(name, type=None):
     if type:
-        found_id = mainDf[mainDf["name"] == name].index[0]
+        found_id = mainDf[mainDf["Name"] == name].index[0]
         for id in indices[found_id][1:]:
             dfToListDf = mainDf.loc[
-                (mainDf["Unnamed: 0"] == id) & (mainDf["type"] == type)
+                (mainDf["Unnamed: 0"] == id) & (mainDf["Type"] == type)
             ]
-            for item in dfToListDf["name"].values.tolist():
+            for item in dfToListDf["Name"].values.tolist():
                 print(item)
     else:
-        found_id = mainDf[mainDf["name"] == name].index[0]
+        found_id = mainDf[mainDf["Name"] == name].index[0]
         for id in indices[found_id][1:]:
-            print(mainDf.iloc[id]["name"])
+            print(mainDf.iloc[id]["Name"])
 
 
 while True:
     name = input("Enter the name of the anime: ")
     number = int(input("Enter number of recommendations: "))
-    print("TV:1 | Movie:2 | Special:3 | OVA:4 | ONA:5 | None:0")
+    print("TV:1 | Movie:2 | Music:3 | OVA:4 | Web:5 | None:0")
     type_ = int(input("Enter number for type of anime: "))
+    print()
 
     #KDtree is used because it is faster in low dimensional datasets
     nbrs = NearestNeighbors(n_neighbors=number, algorithm="kd_tree").fit(fitDf)
@@ -34,29 +35,37 @@ while True:
     try:
         match type_:
             case 0:
+                print("Recommendations: ")
                 animeRec(name)
             case 1:
-                animeRec(name, "TV")
+                print("Recommended TV Shows: ")
+                animeRec(name, "TV   ")
             case 2:
+                print("Recommended Movies: ")
                 animeRec(name, "Movie")
             case 3:
-                animeRec(name, "Special")
+                print("Recommended Music: ")
+                animeRec(name, "Music")
             case 4:
-                animeRec(name, "OVA")
+                print("Recommended OVA's: ")
+                animeRec(name, "OVA  ")
             case 5:
-                animeRec(name, "ONA")
+                print("Recommended Web Shows: ")
+                animeRec(name, "Web  ")
             case _:
                 print("Invalid Input")
     except IndexError:
-        print(f"!!!{name} was not present in the available dataset!!!")
+        print(f"--!\"{name}\" was not present in the available dataset!--")
         again = input("would you like to go again?(y|n): ")
         if again == "n":
             break;
         else:
+            print()
             continue
 
     again = input("would you like to go again(y|n): ")
     if again == "n":
         break;
+    print()
 
-print("===DONE===")
+print("==========DONE==========")
--- dataProcessing.py
+++ dataProcessing.py
@@ -1,28 +1,34 @@
 import numpy as np
 import pandas as pd
 
-animeDf = pd.read_csv("datasets/anime.csv")
+animeDf = pd.read_csv("datasets/Anime.csv")
 
 # the number of episodes is an issue with the dataset as some are incomplete and are unimportant for the reccomendation so they're just dropped, the anime_id is also umimportant so it is also dropped
-animeDf.drop(columns=["anime_id", "episodes"], inplace=True)
+animeDf.drop(columns=["Japanese_name", "Episodes", "Studio", "Release_season", "Release_year", "End_year", "Description", "Content_Warning", "Related_Mange", "Related_anime", "Voice_actors", "staff"], inplace=True)
 
 # convert string numbers to actual, operable numbers
-animeDf["rating"] = pd.to_numeric(animeDf["rating"])
-animeDf["members"] = pd.to_numeric(animeDf["members"])
+animeDf["Rating"] = pd.to_numeric(animeDf["Rating"])
+animeDf["Rank"] = pd.to_numeric(animeDf["Rank"])
 
-# dropping animes with unknown ratings, filling with an average rating could lead to inncaurate recommendations
-animeDf.dropna(how="all", subset=["rating"], inplace=True)
+# dropping animes with unknown ratings and tags, filling with an average rating could lead to inaccurate recommendations and it is not possible to guess tags
+animeDf.dropna(axis=0, how="all", subset=["Tags"], inplace=True)
+animeDf.dropna(axis=0, how="all", subset=["Rating"], inplace=True)
+
+#dropping odd or uncatigorized types
+animeDf.drop(animeDf.loc[animeDf["Type"] == "TV Sp"].index, inplace=True)
+animeDf.drop(animeDf.loc[animeDf["Type"] == "DVD S"].index, inplace=True)
+animeDf.drop(animeDf.loc[animeDf["Type"] == "Other"].index, inplace=True)
 
 # creating dataframe to use to create the fit for the
-fitDataset = animeDf.drop(columns=["name"])
+fitDataset = animeDf.drop(columns=["Name"])
 
 # concatinating the genre and type as dummy vatiables rather than cataigories so that they can be used for calculation
 fitDataset = pd.concat(
-    [fitDataset.drop("genre", axis=1), fitDataset["genre"].str.get_dummies(sep=", ")],
+    [fitDataset.drop("Tags", axis=1), fitDataset["Tags"].str.get_dummies(sep=", ")],
     axis=1,
 )
 fitDataset = pd.concat(
-    [fitDataset.drop("type", axis=1), pd.get_dummies(fitDataset["type"])], axis=1
+    [fitDataset.drop("Type", axis=1), pd.get_dummies(fitDataset["Type"])], axis=1
 )
 
 # create new cleaned dataset for use in the recommendation system
